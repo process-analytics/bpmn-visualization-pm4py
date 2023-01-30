b50f24b5155349c6c31fedf1632af582601777ed
assynour <assy.n.nour@gmail.com>
2023-01-30 17:48:29 +0100

    fix xes import

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 import pm4py
+from pm4py.objects.log.importer.xes import importer as xes_importer
 from enum import Enum
 
 import subprocess #to execute the layout-generator jar file
@@ -11,7 +12,6 @@ class Parameters:
     EVENT_LOG_DISCOVERY = None
     EVENT_LOG_CONFORMANCE = None
     BPMN_MODEL = None
-    PROCESS_TREE = None
     PETRI_NET = None
     IM = None
     FM = None
@@ -19,9 +19,6 @@ class Parameters:
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
-def hello():
-    return "Hello, from Flask!"
 
 @app.route('/discover/inductive-miner', methods=["POST"])
 def discover_inductive_miner():
@@ -29,32 +26,25 @@ def discover_inductive_miner():
     log_stream = request.files.getlist('file')[0].read()
     noise = request.form.get('noise')
 
-    #serialize log, so that we can process it with pm4py
-    serialized_log = ('event_log', log_stream)
-
     #create pm4py event log object
-    Parameters.EVENT_LOG_DISCOVERY = pm4py.deserialize(serialized_log)
+    Parameters.EVENT_LOG_DISCOVERY = xes_importer.deserialize(log_stream)
 
     #discover petri net
     Parameters.PETRI_NET, Parameters.IM, Parameters.FM = pm4py.discover_petri_net_inductive(Parameters.EVENT_LOG_DISCOVERY, noise_threshold=float(noise))
 
     #convert petri net to bpmn
     Parameters.BPMN_MODEL = pm4py.convert_to_bpmn(Parameters.PETRI_NET, Parameters.IM, Parameters.FM)
-    
-    #generate the bpmn xml content from the bpmn graph model
-    #bpmn-file is a tuple: first element is BPMN, second element is the XML content
-    #bpmn_file = pm4py.utils.serialize(Parameters.BPMN_MODEL)
 
     #generate the layout using layout-generators
     '''
-    TO BE FIXED
+    TO BE IMPROVED in future iterations
     '''
     #layout-generators takes a bpmn file, so save the bpmn-file first
     pm4py.write_bpmn(Parameters.BPMN_MODEL, 'result.bpmn')
-
     #generate layout
     subprocess.call(['java', '-jar', 'bpmn-layout-generator-0.1.4-jar-with-dependencies.jar', '--output=./result-with-layout.bpmn', 'result.bpmn'])
-    #get result and send it to frontend
+    
+    #get result and send it
     file = open('result-with-layout.bpmn', 'r')
     bpmn_xml_content = file.read()
     
@@ -66,14 +56,12 @@ def compute_alignment():
     #read xes log
     log_stream = request.files.getlist('file')[0].read()
 
-    #serialize log, so that we can process it with pm4py
-    serialized_log = ('event_log', log_stream)
-
     #create pm4py event log object
-    Parameters.EVENT_LOG_CONFORMANCE = pm4py.deserialize(serialized_log)
+    Parameters.EVENT_LOG_CONFORMANCE = xes_importer.deserialize(log_stream)
+
+    #alignment can be only on done on Petri nets
     if Parameters.PETRI_NET is not None:
         aligned_traces = pm4py.conformance_diagnostics_alignments(Parameters.EVENT_LOG_CONFORMANCE, Parameters.PETRI_NET, Parameters.IM, Parameters.FM)
-        #print(aligned_traces)
         return flask.jsonify(aligned_traces)
     else:
         return flask.Response(response = "BPMN diagram is missing. \n Discover a model and then apply conformance checking", status=404, mimetype='text/xml')
@@ -82,14 +70,10 @@ def compute_alignment():
 def xes_to_csv():
     log_stream = request.files.getlist('file')[0].read()
 
-    #serialize log, so that we can process it with pm4py
-    serialized_log = ('event_log', log_stream)
-
     #create pm4py event log object
-    Parameters.EVENT_LOG_DISCOVERY = pm4py.deserialize(serialized_log)
-    log_df = pm4py.convert_to_dataframe(Parameters.EVENT_LOG_DISCOVERY)
-    return flask.jsonify(log_df.to_html())
-
+    Parameters.EVENT_LOG_DISCOVERY = xes_importer.deserialize(log_stream)
+    log_dataframe = pm4py.convert_to_dataframe(Parameters.EVENT_LOG_DISCOVERY)
+    return flask.jsonify(log_dataframe.to_html())
 
 
 @app.route('/stats/frequency', methods=["GET"])    
@@ -97,7 +81,6 @@ def compute_activity_frequency():
     log_df = pm4py.convert_to_dataframe(Parameters.EVENT_LOG_DISCOVERY)
     activity_freq_df = log_df['concept:name'].value_counts()
     return flask.jsonify(activity_freq_df.to_dict())
-
 
 
 if __name__ == "__main__":
