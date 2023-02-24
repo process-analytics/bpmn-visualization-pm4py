045928c238ee225cdafd1ba1dd1f1977c6887cf3
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-02-24 18:13:44 +0100

    chore(backend-mock-server): provide conformance data
    # Conflicts:
    #	backend-mock-server/src/app.service.ts

--- backend-mock-server/src/app.service.ts
+++ backend-mock-server/src/app.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { diagram } from './assets/diagram';
 
 type FrequencyStats = { [index: string]: number };
+type ConformanceAlignment = Array<{ alignment: Array<Array<string>> }>;
 
 @Injectable()
 export class AppService {
@@ -19,12 +20,12 @@ export class AppService {
     };
   }
 
-  // TODO introduce ConformanceAlignment type
-  getConformanceAlignment(): any {
+  getConformanceAlignment(): ConformanceAlignment {
     return [
-      { alignment: ['Assign Approver', 'Approve Invoice'] },
-      { alignment: ['Assign Approver', 'Approve Invoice'] },
-      { alignment: ['Approve Invoice', 'Approve Invoice'] },
+      { alignment: [['Approve Invoice', 'Assign Approver']] },
+      { alignment: [['Assign Approver', 'Approve Invoice']] },
+      { alignment: [['Assign Approver', 'Approve Invoice']] },
+      { alignment: [['Approve Invoice', 'Approve Invoice']] },
     ];
   }
 
