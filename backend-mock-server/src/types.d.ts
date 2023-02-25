df772fc168af1ea5de02fc266dc41159c6f2589d
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-02-25 14:01:55 +0100

    remove type file for txt resources

--- backend-mock-server/src/types.d.ts
+++
@@ -1,9 +0,0 @@
-// declare module "template.html!text" {
-//     var html: number;
-//     export default x;
-// }
-
-declare module '*.bpmn!text' {
-  const content: string;
-  export default content;
-}
